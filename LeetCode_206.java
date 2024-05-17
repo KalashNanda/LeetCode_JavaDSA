3056fbe7f0ef398c267be267c1f6c2742f5f2227
Kalash Nanda <121033626+KalashNanda@users.noreply.github.com>
2024-05-17 23:16:38 +0530

    Add files via upload

---
+++ LeetCode_206.java
@@ -0,0 +1,30 @@
+package LeetCode;
+// Iterative Method
+class ReverseLinkedList1 {
+//    public ListNode reverseList(ListNode head) {
+//        ListNode curr = head;
+//        ListNode Next = head;
+//        ListNode prev = null;
+//        while(curr!=null){
+//            Next = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = Next;
+//        }
+//        return prev;
+//    }
+}
+
+// Recursive Method
+class ReverseLinkedList2 {
+//    public ListNode reverseList(ListNode head) {
+//        if(head==null || head.next==null) return head;
+//        ListNode a = head.next;
+//        ListNode newHead = reverseList(a);
+//        a.next = head;
+//        head.next = null;
+//        return newHead;
+//    }
+}
+public class LeetCode_206 {
+}
